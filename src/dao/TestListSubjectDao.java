775b8e750db72721b1af1a6e6d49b56a3eaba66d
kazura1423 <knz2374559@stu.o-hara.ac.jp>
2024-04-26 00:52:47 +0900

    TestListSubjectDao進捗

--- src/dao/TestListSubjectDao.java
+++ src/dao/TestListSubjectDao.java
@@ -43,12 +43,12 @@ public class TestListSubjectDao {
 
 
 	/**
-	 * filterメソッド 学校を指定して科目の一覧を取得する
+	 * filterメソッド 学校を指定して科目別テスト結果の一覧を取得する
 	 *
-	 * @return 科目のリスト:List<Subject> 存在しない場合は0件のリスト
+	 * @return 科目別テスト結果のリスト:List<TestListSubject> 存在しない場合は0件のリスト
 	 * @throws Exception
 	 */
-	public List<TestListSubject> filter(int entYear, String classNum, Subject subject School school) throws Exception {
+	public List<TestListSubject> filter(int entYear, String classNum, Subject subject, School school) throws Exception {
 		List<TestListSubject> list = new ArrayList<>();
 
 		//データベースへのコネクションを確立
@@ -66,6 +66,7 @@ public class TestListSubjectDao {
 		try{
 
 			//プリペアードステートメントにSQL文をセット
+			//絞り込みは入学年度、クラス、科目の組み合わせの8パターン必要
 			statement = connection.prepareStatement(baseSql + condition + order);
 
 			//プレースホルダー（？の部分）に値を設定
@@ -79,9 +80,9 @@ public class TestListSubjectDao {
 				SchoolDao schoolDao = new SchoolDao();
 
 				//結果をリストに格納
+				//入学年度、クラス、学生番号、氏名、１回目、２回目の点数を格納
 				while (rSet.next()) {
-					Subject subject = new Subject();
-
+					TestListSubject testlistsubject = new TestListSubject();
 					subject.setSchool(schoolDao.get(rSet.getString("school_cd")));
 					subject.setCd(rSet.getString("subject_co"));
 					subject.setName(rSet.getString("name"));
@@ -109,6 +110,12 @@ public class TestListSubjectDao {
 		}
 	}
 	return list;
+	}
+
+
+	private Connection getConnection() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
 	}
 
 
