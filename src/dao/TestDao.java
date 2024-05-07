c6b03e83924faee2bdbe380e530477ee80073156
kazura1423 <knz2374559@stu.o-hara.ac.jp>
2024-05-07 15:06:54 +0900

    testDao 修正

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -230,12 +230,11 @@ public class TestDao extends Dao {
 					statement.setString(6, test.getClassNum());
 					} else {
 						statement = connection.prepareStatement("update test set point=? where student_no=? and subject_cd=? and school_cd=? and no=?");
-						statement.setString(1,test.getStudent().getNo() );
-						statement.setString(2, test.getSubject().getCd());
-						statement.setString(3, test.getSchool().getCd());
-						statement.setInt(4, test.getNo());
-						statement.setInt(5, test.getPoint());
-						statement.setString(6, test.getClassNum());
+						statement.setInt(1, test.getPoint());
+						statement.setString(2,test.getStudent().getNo() );
+						statement.setString(3, test.getSubject().getCd());
+						statement.setString(4, test.getSchool().getCd());
+						statement.setInt(5, test.getNo());
 
 					}
 
