37529b0da6bae1a8e3c6e8b387b7ef9fbaacb328
kazura1423 <knz2374559@stu.o-hara.ac.jp>
2024-05-07 13:58:00 +0900

    修正

--- src/scoremanager/main/TestRegistAction.java
+++ src/scoremanager/main/TestRegistAction.java
@@ -68,7 +68,6 @@ public class TestRegistAction extends Action {
 			req.setAttribute("f2", classNum);
 			req.setAttribute("f3", subjectCd);
 			req.setAttribute("f4", Num);
-			System.out.print(list.get(1).getPoint());
 
 
 			}//1つでも値が入力されていたとき…入力が不足しているとき
